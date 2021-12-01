0ea662938e9faeea34d58544a8341493616435a4
marcusrjb <marcus.britto@gmail.com>
2021-12-01 15:22:14 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/marcusrjb/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.marcusrjb.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcusrjb.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
